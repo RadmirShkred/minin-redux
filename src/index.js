3770f3b8100b2e059e607aaae94e52dfe19777cf
radmir-shkred <psitecton@gmail.com>
2020-05-21 14:03:01 +0300

    essence redux

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware, compose} from "redux";
+import rootReducer from "./redux/rootReducer";
+import {Provider} from "react-redux";
+import reduxThunk from 'redux-thunk';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const loggerMiddleware = store => next => action => {
+    const result = next(action);
+    console.log('Middleware', store.getState());
+    return result;
+};
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk)));
+
+const app = (
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(app, document.getElementById('root'));
+
