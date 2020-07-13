b86bc6e6dfdc1a4f74ba8462034345f2db419fcf
radmir-shkred <psitecon@gmail.com>
2020-07-13 20:26:18 +0300

    Updates

---
+++ precache-manifest.4a24bcae08b7737fdbf81d858bf32c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee53295e7567bfd37559562f2c6cd04",
+    "url": "/minin-redux/index.html"
+  },
+  {
+    "revision": "7eab9d8c7b74b086ecf2",
+    "url": "/minin-redux/static/css/main.71563f84.chunk.css"
+  },
+  {
+    "revision": "3a54dfa8f14f20a9f01e",
+    "url": "/minin-redux/static/js/2.5079870c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/minin-redux/static/js/2.5079870c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eab9d8c7b74b086ecf2",
+    "url": "/minin-redux/static/js/main.d634ffcb.chunk.js"
+  },
+  {
+    "revision": "1bfca16f4559c135f369",
+    "url": "/minin-redux/static/js/runtime-main.48de053b.js"
+  }
+]);
